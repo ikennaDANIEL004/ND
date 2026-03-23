38191f013948cf1ddbde39db91518f55f530da09
Daniel <ikennadaniel004@gmail.com>
2026-03-23 23:19:12 +0100

    first commit

--- uncle_shop/uncle_shop/settings.py
+++ uncle_shop/uncle_shop/settings.py
@@ -125,3 +125,6 @@ import os
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+CSRF_TRUSTED_ORIGINS = [
+    "https://nd-production-42eb.up.railway.app",
+]

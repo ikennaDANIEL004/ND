6a174ed62ccfc826268d0e2ba6e8ea39c557b4fe
Daniel <ikennadaniel004@gmail.com>
2026-05-12 22:42:31 +0100

    more

--- uncle_shop/uncle_shop/settings.py
+++ uncle_shop/uncle_shop/settings.py
@@ -151,7 +151,7 @@ EMAIL_BACKEND = os.getenv(
 EMAIL_HOST = os.getenv("EMAIL_HOST", "smtp.mail.yahoo.com")
 EMAIL_PORT = int(os.getenv("EMAIL_PORT", "587"))
 EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS", "True") == "True"
-EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER", "")
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER", "ndubueze77@yahoo.com")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD", "")
 DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL", "ndubueze77@yahoo.com")
 CONTACT_NOTIFICATION_EMAIL = os.getenv("CONTACT_NOTIFICATION_EMAIL", "ndubueze77@yahoo.com")
